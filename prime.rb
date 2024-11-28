5f7dcb76a6d7cf47943fd7d93840d55f8ca21323
Aalekh Modgil <aalekhmodgil8@gmail.com>
2024-11-28 11:36:24 +0530

    Complete prime.rb

--- prime.rb
+++ prime.rb
@@ -1,5 +1,20 @@
 print "Enter a number :- "
 n = gets.to_i
 
-for i in 1..n do
-  
+if n < 2 # n == 0 || n == 1
+    puts "No prime number in given range"
+    exit
+end
+
+for i in 2..n do
+    flag = 1
+    for j in 2..Math.sqrt(i).to_i do
+        if i % j == 0
+            flag = 0
+            break
+        end
+    end
+    if flag == 1
+        print "#{i} "
+    end
+end
